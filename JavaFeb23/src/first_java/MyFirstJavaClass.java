b71864f1d34b2eeb4c91f505dbe0904c251a9a2f
tabasumshaki <stabasumzaman@gmail.com>
2023-03-16 00:18:52 -0400

    just deleted learn java project and added the package of that project to java feb 23.

---
+++ JavaFeb23/src/first_java/MyFirstJavaClass.java
@@ -0,0 +1,14 @@
+package first_java;
+
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("I am learning Java!");
+		
+		System.out.println("its 3pm already");
+		
+		//fgknfsdkjjask;
+
+	}
+
+}
